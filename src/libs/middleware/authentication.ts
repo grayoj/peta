85b20dbc33e1c0ce9d32d8b7792ee68d38593e3c
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2024-02-03 13:45:22 +0100

    Feature: verify user's authentication state.

---
+++ src/libs/middleware/authentication.ts
@@ -0,0 +1,43 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { JwtSecret } from "../../helpers/constants";
+
+type User = {
+  id: string;
+  email: string;
+  firstname: string;
+  lastname: string;
+};
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: User;
+    }
+  }
+}
+
+export function authenticate(
+  req: Request,
+  res: Response<any, Record<string, any>>,
+  next: NextFunction,
+): void {
+  const token = req.header("Authorization");
+
+  if (!token) {
+    res.status(401).json({ message: "No token provided" });
+    return;
+  }
+
+  try {
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_SECRET || JwtSecret,
+    ) as User;
+    req.user = decoded;
+    next();
+  } catch (error) {
+    console.error("Error verifying token:", error);
+    res.status(401).json({ message: "Invalid token" });
+  }
+}
