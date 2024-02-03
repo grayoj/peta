5bf8acb02bcbd554b69ff88f7ed4dd536aad246c
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2024-02-03 13:46:58 +0100

    Refactor: Handle errors better on authentication.

--- src/controller/AuthController.ts
+++ src/controller/AuthController.ts
@@ -1,33 +1,54 @@
-import { Request, Response } from "express";
+import * as express from "express";
 import { AuthService } from "../services/AuthService";
+import { UserModel } from "../models/UserModel";
 
 export class AuthController {
-  static async signup(req: Request, res: Response): Promise<void> {
+  static async signup(
+    req: express.Request,
+    res: express.Response,
+  ): Promise<void> {
     try {
       const { firstname, lastname, username, email, password } = req.body;
-      const token = await AuthService.signup(
-        firstname,
-        lastname,
-        username,
-        email,
-        password,
-      );
-      res.status(201).json({ token });
+
+      const existingUser = await UserModel.findOne({ email });
+      if (existingUser) {
+        res.status(400).json({ message: "User already exists" });
+        return;
+      }
+
+      await AuthService.signup(firstname, lastname, username, email, password);
+
+      res.status(201).json({ message: "Signup successful" });
     } catch (error) {
       console.error("Error signing up:", error);
       res.status(500).json({ message: "Internal server error" });
     }
   }
 
-  static async login(req: Request, res: Response): Promise<void> {
+  static async login(
+    req: express.Request,
+    res: express.Response,
+  ): Promise<void> {
     try {
       const { email, password } = req.body;
       const token = await AuthService.login(email, password);
+      const user = await UserModel.findOne({ email });
+
+      if (!user) {
+        res.status(401).json({ message: "User not found" });
+        return;
+      }
+
+      const userId = user._id;
+
       if (!token) {
         res.status(401).json({ message: "Invalid credentials" });
         return;
       }
-      res.status(200).json({ token, email });
+
+      const { firstname, lastname } = user;
+
+      res.status(200).json({ userId, token, firstname, lastname, email });
     } catch (error) {
       console.error("Error logging in:", error);
       res.status(500).json({ message: "Internal server error" });
